5de3d61c1db5bb9f811b7779a033168134c62936
Chris McKissack <75745188+ChrisMcK1@users.noreply.github.com>
2021-01-23 20:56:29 -0600

    Update listIterationTest.py
    continuing attempt to check for playable rolls

--- listIterationTest.py
+++ listIterationTest.py
@@ -1,9 +1,23 @@
-theBoard = ['1', 'X', '3','4',\
-            'X', 'X', 'X', 'X', '9', \
+
+theBoard = [1, 'X', 3, 4,\
+            'X', 'X', 'X', 'X', 9, \
             'X']
+testList = [1, 2, 3, 4, 5, 6]
 
 diceRoll = 6
 
+
+for i in theBoard[0:(diceRoll-1)]:
+    try:
+        if i == diceRoll:
+            print(i)
+
+        else:
+            print(i+1)
+    except ValueError:
+        continue
+    except TypeError:
+        continue
 
 def combinations(values):
     if len(values) == 0:
@@ -16,18 +30,5 @@ def combinations(values):
 print (combinations([1]))  # Expecting: [[1]]
 print (combinations([1, 2])) # Expecting: [1], [2], [1 ,2]
 print (combinations([1, 2, 3])) #Expecting: [[1], [2], [3], [1, 2], [2, 3], [1, 2, 3]]
-
-for pieces in theBoard[0:(diceRoll)]:
-    try:
-        if int(pieces) == diceRoll:
-            print(pieces)
-        if int(theBoard[0]) + int(theBoard[2]) == 4:
-            print('winner!')
-
-        else:
-            print('no hit')
-    except ValueError:
-        continue
-
 
                 
